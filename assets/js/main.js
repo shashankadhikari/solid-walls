9cbb670eb97f6b6c712614e1cfeb20544bf26d97
unknown <shashank.adhikari4152@gmail.com>
2021-02-23 22:54:38 +0545

    Crousel managed

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,20 @@
     }
   });
 
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+  });
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-menu, .btn-book').addClass('animate__animated animate__fadeInUp');
+  });
+
 
   
 
